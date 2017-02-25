a02cc190298d17418241b90d818b51c996a29e20
Rogerio Nascimento <rnascim@gmail.com>
2017-02-25 17:29:08 +0000

    ./rd.sh

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,55 +12,6 @@ class CompaniesController < ApplicationController
   def show
   end
 
-  # GET /companies/new
-  def new
-    @company = Company.new
-  end
-
-  # GET /companies/1/edit
-  def edit
-  end
-
-  # POST /companies
-  # POST /companies.json
-  def create
-    @company = Company.new(company_params)
-
-    respond_to do |format|
-      if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
-        format.json { render :show, status: :created, location: @company }
-      else
-        format.html { render :new }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /companies/1
-  # PATCH/PUT /companies/1.json
-  def update
-    respond_to do |format|
-      if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
-        format.json { render :show, status: :ok, location: @company }
-      else
-        format.html { render :edit }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /companies/1
-  # DELETE /companies/1.json
-  def destroy
-    @company.destroy
-    respond_to do |format|
-      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_company
