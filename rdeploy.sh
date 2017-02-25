3fd930aae2edb758d8520c389bd643a9a1af99c9
Rogerio Nascimento <rnascim@gmail.com>
2017-02-25 15:31:59 +0000

    Test Message

--- rdeploy.sh
+++ rdeploy.sh
@@ -3,11 +3,12 @@ git add .
 git commit -am"Test Message"
 #git push origin master
 targString=$( git push origin master | awk '{print $2}')
-echo $targString
+echo 'push: '$targString
 exit 0  
 
 ssh rogerionascimento.com << EOF
   sudo su -
   cd ~/blog
   ./deploy.sh
+  exit  
 EOF
