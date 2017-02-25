3261c3427564c4a9f06cd337add73d1a1170aba6
Rogerio Nascimento <rnascim@gmail.com>
2017-02-25 15:38:05 +0000

    Test Message

--- rdeploy.sh
+++ rdeploy.sh
@@ -3,7 +3,8 @@ git add .
 git commit -am"Test Message"
 targString=$( git commit -am"Test Message" | awk '{print $0}')
 
-if [[ $targString == *"nothing to commit"*]]; then
+if [[ $targString == *"nothing to commit"* ]]
+then
   echo -e "Nothing to do"
   exit 0
 fi
