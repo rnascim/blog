b7eab089af6637ab2cf50c787dcce95fb9c8fc93
Rogerio Nascimento <rnascim@gmail.com>
2017-02-25 15:37:28 +0000

    Test Message

--- rdeploy.sh
+++ rdeploy.sh
@@ -2,9 +2,13 @@
 git add . 
 git commit -am"Test Message"
 targString=$( git commit -am"Test Message" | awk '{print $0}')
+
+if [[ $targString == *"nothing to commit"*]]; then
+  echo -e "Nothing to do"
+  exit 0
+fi
+
 git push origin master
-echo 'push: '$targString
-exit 0  
 
 ssh rogerionascimento.com << EOF
   sudo su -
